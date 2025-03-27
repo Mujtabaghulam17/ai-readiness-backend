c37ca84e76c675afcab2bd0eaf849d813e10f610
Mujtabaghulam17 <109437580+Mujtabaghulam17@users.noreply.github.com>
2025-03-27 22:45:33 +0100

    Update Result.js

--- models/Result.js
+++ models/Result.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-const resultSchema = new mongoose.Schema({
-  email: { type: String, required: false },
-  answers: { type: mongoose.Schema.Types.Mixed, required: true },
-  average: { type: Number, required: true },
-  createdAt: {
-    type: Date,
-    default: Date.now,
+const resultSchema = new mongoose.Schema(
+  {
+    email: String,
+    answers: mongoose.Schema.Types.Mixed, // ← laat elk JSON toe
+    average: Number,
   },
-});
+  { strict: false } // ← belangrijk: laat extra velden toe
+);
 
 module.exports = mongoose.model('Result', resultSchema);
